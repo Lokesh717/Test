c648c0f748a29daed96d3161b235d0409e39e992
Lokesh717 <bl.selenium7@gmail.com>
2017-04-26 01:27:00 -0400

    Project

---
+++ project/Importdata_excel/src/BeanObjects/Clarity_UserDetails.java
@@ -0,0 +1,26 @@
+package BeanObjects;
+
+public class Clarity_UserDetails {
+
+	String uName;	
+	String passWord;
+	String urlPath;
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getUrlPath() {
+		return urlPath;
+	}
+	public void setUrlPath(String urlPath) {
+		this.urlPath = urlPath;
+	}
+}
